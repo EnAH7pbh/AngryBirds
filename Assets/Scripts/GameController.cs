19f7bc5e37deff8ad07adebc603a450f38fa06f3
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2019-11-26 19:53:42 +0700

    update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     public SlingShooter slingshooter;
     public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    public bool _isGameEnded = false;
     // Start is called before the first frame update
-    void Start()
-    {
-        slingshooter.InitiateBird(Birds[0]);
+    void Start () {
+        for (int i = 0; i < Birds.Count; i++) {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+        }
+        for (int i = 0; i < Enemies.Count; i++) {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+        slingshooter.InitiateBird (Birds[0]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChangeBird () {
+        if (_isGameEnded) {
+            return;
+        }
+        Birds.RemoveAt (0);
+        if (Birds.Count > 0) {
+            slingshooter.InitiateBird (Birds[0]);
+        }
+    }
+
+    public void CheckGameEnd (GameObject DestroyedEnemy) {
+        for (int i = 0; i < Enemies.Count; i++) {
+            if (Enemies[i].gameObject == DestroyedEnemy) {
+                Enemies.RemoveAt (i);
+                break;
+            }
+        }
+        if (Enemies.Count == 0) {
+            _isGameEnded = true;
+        }
     }
 }
